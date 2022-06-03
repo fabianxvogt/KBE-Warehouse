1d3f4992c6ed49ab543d6c59b9491a3f844daada
Fabian Vogt <fabianxvogt@gmail.com>
2022-06-03 14:31:58 +0200

    first successful build

--- BE/src/main/java/checkout/CheckoutOption.java
+++
@@ -1,3 +0,0 @@
-package checkout;
-class CheckoutOption {
-}
--- BE/src/main/java/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface UserRepository extends JpaRepository<User, Long> {
-
-}
